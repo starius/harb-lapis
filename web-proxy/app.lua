02f99871669df731ec018c49a747cea9f9d6650e
Boris Nagaev <bnagaev@gmail.com>
2014-10-15 13:28:01 +0400

    implement web-proxy

--- web-proxy/app.lua
+++ web-proxy/app.lua
@@ -1,8 +1,21 @@
 local lapis = require("lapis")
 local app = lapis.Application()
+local http = require("lapis.nginx.http")
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+app:get("/", function(self)
+  return [[
+    <form method="POST" action="/geturl">
+      <input type="text" value="http://ip4.me/" name="url" />
+      <input type="submit" value="Get" />
+    </form>
+    ]]
+end)
+
+app:post("/geturl", function(self)
+  local url = self.req.params_post.url
+  print(url)
+  local body, status_code, headers = http.simple(url)
+  return body
 end)
 
 return app
